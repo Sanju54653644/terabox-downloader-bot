3c1d8f3b82388b301d7e96e48281e5af2f26e7bb
Sanjoy mahato <124846542+Sanju54653644@users.noreply.github.com>
2024-03-30 11:24:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,5 +10,5 @@ PORT = 6379  # redis port
 PASSWORD = ""  # redis password
 
 PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
-COOKIE = ""  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
+COOKIE = "PANWEB=1; csrfToken=zcB-8zYpUSuGJYARruP6ciKa; lang=en; TSID=UG8Ux11m0tQJhha2h6AYF6HoRuElxvMU;_bid_n=18c0b12d393d1a88c44207;_ga=GA1.1.534448201.1700992834;_stripe_mid=63de2b36-0aa9-48d3-6049-d92a35378933041de1; ndus-Y-Rose7teHuivLrNA0f1oAiHAzBWcLRClovVHKTb;browserid=cP5XWdCGKn9XXeBSULS9_aCyY3XzUgKTIo5xMGHMbeYH3WJwFilvauWfTg8=;_ga_06ZNKL8C2E-GS1.1.1707931403.23.1.1707931712.60.0.0; ndut_fmt=1200AD9E2CF492FD104EBC622EF505225618603C27A46F87CE88644938DC8353"  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (don't use this)
 ADMINS = [1317173146]
